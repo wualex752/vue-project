ffc95912331f058a04eeeb35e13e91fd7423592b
alex792 <wualex792@gmail.com>
2020-03-12 17:56:29 +0800

    Menu

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ cd dist
 git init
 git add -A
 git commit -m 'Deploy'
-git push -f https://github.com/wualex752/vue-project.git master:gh-pages
+git push -f https://wualex752@github.com/wualex752/vue-project.git master:gh-pages
 cd -
--- src/views/Index/Home.vue
+++ src/views/Index/Home.vue
@@ -4,21 +4,36 @@
 
     <footer>
       <ul>
-        <li>
-          <div></div>
-          <router-link :to="{ name: 'game', params: {vendor: 'sport'} }">首頁</router-link>
+        <li 
+          @click="setActive('index')"
+          :class="{ active: activeItem == 'index' }">
+          <router-link :to="{ name: 'game', params: {vendor: 'sport'} }">
+            <div></div>
+            首頁
+          </router-link>
         </li>
-        <li>
-          <div></div>
-          <router-link :to="{ name: 'discount', params: {category: 'all'} }">優惠</router-link>
+        <li
+          @click="setActive('discount')"
+          :class="{ active: activeItem == 'discount' }">
+          <router-link :to="{ name: 'discount', params: {category: 'all'} }">
+            <div></div>優惠
+          </router-link>
         </li>
-        <li>
-          <div></div>
-          <router-link :to="{ name: 'customer' }">客服</router-link>
+        <li
+          @click="setActive('customer')"
+          :class="{ active: activeItem == 'customer' }">
+          <router-link :to="{ name: 'customer' }">
+            <div></div>
+            客服
+          </router-link>
         </li>
-        <li>
-          <div></div>
-          <router-link :to="{ name: 'myCenter' }">我的</router-link>
+        <li
+          @click="setActive('myCenter')"
+          :class="{ active: activeItem == 'myCenter' }">
+          <router-link :to="{ name: 'myCenter' }">
+            <div></div>
+            我的
+          </router-link>
         </li>
       </ul>
     </footer>
@@ -30,8 +45,14 @@ export default {
   name: "Home",
   data() {
     return {
-      active: 0
+      active: 0,
+      activeItem: 'index'
     };
+  },
+  methods: {
+    setActive(item) {
+      this.activeItem = item;
+    }
   }
 };
 </script>
@@ -51,9 +72,9 @@ footer {
     flex-direction: row;
     justify-content: space-evenly;
     align-items: center;
-    border-top-left-radius: 0.4rem;
-    border-top-right-radius: 0.4rem;
-    box-shadow: 0 0 0.1rem 0 rgba(0, 0, 0, 0.2);
+    border-top-left-radius: 6.4rem;
+    border-top-right-radius: 6.4rem;
+    box-shadow: 0 0 1.2rem 0 rgba(0, 0, 0, 0.2);
     background: hsla(0, 0%, 100%, 0.9);
     backdrop-filter: blur(0.2rem);
     width: 100%;
@@ -63,6 +84,42 @@ footer {
     flex: 1;
     position: relative;
     font-size: 2.8rem;
+    transition: all .3s;
+
+    &:first-child div {
+      background-position: -1rem -1rem;
+    }
+
+    &:nth-child(2) div {
+      background-position: -12rem -1rem;
+    }
+    &:nth-child(3) div {
+      background-position: -24rem -1rem;
+    }
+    &:nth-child(4) div {
+      background-position: -45rem -1rem;
+    }
+
+    &.active {
+
+      &:first-child div {
+        background-position: -56rem -1rem;
+      }
+      &:nth-child(2) div {
+        background-position: -68rem -1rem;
+      }
+      &:nth-child(3) div {
+        background-position: -79rem -1rem;
+      }
+      &:nth-child(4) div {
+        background-position: -90rem -1rem;
+      }
+      a {
+        color: #d2b79c;
+      }
+
+    }
+
     div {
       background-image: url("https://h5static.0am08m.com/assets/v4_common/tabBar.png.webp?version=1583773880435");
       background-repeat: no-repeat;
@@ -70,22 +127,10 @@ footer {
       height: 9rem;
       margin: 0 auto;
       background-size: 111rem;
-
-      &:first-child {
-        background-position: -1rem -1rem;
-      }
-      &:nth-child(2) {
-        background-position: -1rem -1rem;
-      }
-      &:nth-child(3) {
-        background-position: -1rem -1rem;
-      }
-      &:nth-child(4) {
-        background-position: -1rem -1rem;
-      }
     }
+
     a {
-      font-size: 0.2rem;
+      font-size: 3.6rem;
     }
   }
 
